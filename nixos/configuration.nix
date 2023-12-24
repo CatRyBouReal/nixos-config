a249d2d2d3ead067c66a9128ba6d2ad994d319c6
Anish Vundela <anishreddyvundela@proton.me>
2023-12-24 10:41:44 +0000

    Added pipewire and sound support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -132,6 +132,17 @@
     ];
   };
 
+  # Enable pipewire
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   users.users = {
     catrybou = {
       initialPassword = "correcthorsebatterystaple";
